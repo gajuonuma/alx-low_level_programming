cd83f75866cd81079bf341724f25b0e0e0757a67
gajuonuma <gajuonuma@gmail.com>
2022-04-11 11:18:56 -0700

    3 in variables

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,11 +12,11 @@ char upper_case;
 
 for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 {
-putchar(upper_case);
+putchar(lower_case);
 }
 for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
 {
-putchar(lower_case);
+putchar(upper_case);
 }
 
 putchar('\n');
